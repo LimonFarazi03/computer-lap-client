b8caa8e9b478f9c553cdb8fcd99468cd1f26e15f
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-26 12:41:33 +0600

    notFound page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import Navbar from "./components/Navbar/Navbar";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
     <Navbar></Navbar>
        <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/blogs" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,160 @@
-import React from 'react';
-import './Blogs.css';
+import React from "react";
+import "./Blogs.css";
+import jsVSnode from "../../Images/jsVSnode.jpg";
 
 const Blogs = () => {
   return (
-    <div>
-      
+    <div className="container text-center mt-3 mb-5">
+      <h1 className="text-uppercase fw-bold">Blogs</h1>
+
+      <div className="blog-img mt-5 rounded p-5 bg-secondary d-flex justify-content-start align-items-center">
+        <div>
+          <img src={jsVSnode} alt="" />
+        </div>
+        <div>
+          <h1 className="fs-3 mb-3">1. Difference between javascript and node js ?</h1>
+          <div className="text-start text-field text-justify d-flex justify-content-start align-items-center">
+            <div>
+              <p>
+              <h1 className="fs-5">Javascript</h1>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <p>
+              <h1 className="fs-5">NodeJS</h1>
+                NodeJS is a Javascript runtime environment. 2) We can run
+                Javascript outside the browser with the help of NodeJS. 3) It is
+                mostly used on the server-side. 4) Nodejs does not have
+                capability to add HTML tags. 5) V8 is the Javascript engine
+                inside of node.js that parses and runs Javascript. 6) Nodejs is
+                used in server-side development. 7) Some of the Nodejs modules
+                are Lodash, express etc. These modules are to be imported from
+                npm. 8) Nodejs is written in C, C++ and Javascript.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="blog-img mt-5 rounded p-5 bg-secondary d-flex justify-content-start align-items-center">
+        <div>
+          <img src={jsVSnode} alt="" />
+        </div>
+        <div>
+          <h1 className="fs-3 mb-3">1. Difference between javascript and node js ?</h1>
+          <div className="text-start text-field text-justify d-flex justify-content-start align-items-center">
+            <div>
+              <p>
+              <h1 className="fs-5">Javascript</h1>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <p>
+              <h1 className="fs-5">NodeJS</h1>
+                NodeJS is a Javascript runtime environment. 2) We can run
+                Javascript outside the browser with the help of NodeJS. 3) It is
+                mostly used on the server-side. 4) Nodejs does not have
+                capability to add HTML tags. 5) V8 is the Javascript engine
+                inside of node.js that parses and runs Javascript. 6) Nodejs is
+                used in server-side development. 7) Some of the Nodejs modules
+                are Lodash, express etc. These modules are to be imported from
+                npm. 8) Nodejs is written in C, C++ and Javascript.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="blog-img mt-5 rounded p-5 bg-secondary d-flex justify-content-start align-items-center">
+        <div>
+          <img src={jsVSnode} alt="" />
+        </div>
+        <div>
+          <h1 className="fs-3 mb-3">1. Difference between javascript and node js ?</h1>
+          <div className="text-start text-field text-justify d-flex justify-content-start align-items-center">
+            <div>
+              <p>
+              <h1 className="fs-5">Javascript</h1>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <p>
+              <h1 className="fs-5">NodeJS</h1>
+                NodeJS is a Javascript runtime environment. 2) We can run
+                Javascript outside the browser with the help of NodeJS. 3) It is
+                mostly used on the server-side. 4) Nodejs does not have
+                capability to add HTML tags. 5) V8 is the Javascript engine
+                inside of node.js that parses and runs Javascript. 6) Nodejs is
+                used in server-side development. 7) Some of the Nodejs modules
+                are Lodash, express etc. These modules are to be imported from
+                npm. 8) Nodejs is written in C, C++ and Javascript.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="blog-img mt-5 rounded p-5 bg-secondary d-flex justify-content-start align-items-center">
+        <div>
+          <img src={jsVSnode} alt="" />
+        </div>
+        <div>
+          <h1 className="fs-3 mb-3">1. Difference between javascript and node js ?</h1>
+          <div className="text-start text-field text-justify d-flex justify-content-start align-items-center">
+            <div>
+              <p>
+              <h1 className="fs-5">Javascript</h1>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <p>
+              <h1 className="fs-5">NodeJS</h1>
+                NodeJS is a Javascript runtime environment. 2) We can run
+                Javascript outside the browser with the help of NodeJS. 3) It is
+                mostly used on the server-side. 4) Nodejs does not have
+                capability to add HTML tags. 5) V8 is the Javascript engine
+                inside of node.js that parses and runs Javascript. 6) Nodejs is
+                used in server-side development. 7) Some of the Nodejs modules
+                are Lodash, express etc. These modules are to be imported from
+                npm. 8) Nodejs is written in C, C++ and Javascript.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
