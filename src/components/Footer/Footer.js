299e568c0633161dc403aa97097c04f7bd759b2c
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-31 21:03:30 +0600

    img added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import footerImg from "../../Image/logo.png";
 import { AiFillFacebook } from "react-icons/ai";
 
 const Footer = () => {
@@ -9,20 +10,32 @@ const Footer = () => {
       <div className="container text-md-left">
         <div className="row text-md-left">
           <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h5 className=" text-uppercase mb-4 w-bold text-warning">
-              Company Name
-            </h5>
+            <div className="footerImg">
+              <img src={footerImg} alt="" />
+            </div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
               voluptatibus est ad placeat, fugiat, vero inventore magni
               doloribus enim maxime reprehenderit asperiores sapiente voluptas
               perferendis temporibus sint nihil atque. Minus.
             </p>
-            <div className='svgIcon'>
-            <a href="##"> <AiFillFacebook/> </a>
-            <a href="##"> <AiFillFacebook/> </a>
-            <a href="##"> <AiFillFacebook/> </a>
-            <a href="##"> <AiFillFacebook/> </a>
+            <div className="svgIcon">
+              <a href="##">
+                {" "}
+                <AiFillFacebook />{" "}
+              </a>
+              <a href="##">
+                {" "}
+                <AiFillFacebook />{" "}
+              </a>
+              <a href="##">
+                {" "}
+                <AiFillFacebook />{" "}
+              </a>
+              <a href="##">
+                {" "}
+                <AiFillFacebook />{" "}
+              </a>
             </div>
           </div>
           <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
@@ -30,12 +43,12 @@ const Footer = () => {
               Products
             </h5>
             <div>
-               <p>Blogs</p>
-               <p>Pricing</p>
-               <p>Sales</p>
-               <p>Tickets</p>
-               <p>Certification</p>
-               <p>Customer Service</p>
+              <p>Blogs</p>
+              <p>Pricing</p>
+              <p>Sales</p>
+              <p>Tickets</p>
+              <p>Certification</p>
+              <p>Customer Service</p>
             </div>
           </div>
           <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
@@ -43,18 +56,16 @@ const Footer = () => {
               Useful Links
             </h5>
             <div>
-               <p>Blogs</p>
-               <p>Pricing</p>
-               <p>Sales</p>
-               <p>Tickets</p>
-               <p>Certification</p>
-               <p>Customer Service</p>
+              <p>Blogs</p>
+              <p>Pricing</p>
+              <p>Sales</p>
+              <p>Tickets</p>
+              <p>Certification</p>
+              <p>Customer Service</p>
             </div>
           </div>
           <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h5 className="text-uppercase mb-4 w-bold text-warning">
-              Address
-            </h5>
+            <h5 className="text-uppercase mb-4 w-bold text-warning">Address</h5>
             <p>127, Westwood Land</p>
             <p>DA15 9PS, Sidecup</p>
             <p>London, UK</p>
