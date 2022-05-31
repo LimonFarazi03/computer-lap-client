b002d8567bec74ec96273b302d6a34d3dd19e4ea
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-31 21:20:15 +0600

    about us content added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import "./About.css";
+import aboutUsImg from "../../Image/aboutUs.jpg";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is about components</h1>
+    <div className="container d-flex justify-content-center align-items-center my-5">
+      <div className='aboutImg'>
+      <img src={aboutUsImg} alt="" />
+      </div>
+      <div>
+        <h1 className='mb-3'>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, in
+          quos est veritatis at nostrum voluptate sunt ut dicta! Ad consequuntur
+          quos iure, voluptas laborum quis perferendis ex illo deserunt?
+        </p>
+        <div className="btn btn-primary text-white">Learn More</div>
+      </div>
     </div>
   );
 };
