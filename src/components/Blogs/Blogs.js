4caeb5634908371e07268afad17f1887a0b8c2b8
MD Limon Farazi <abidislamabir@gmail.com>
2022-05-31 20:58:28 +0600

    footer

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -5,8 +5,10 @@ import vsImg from "../../Image/jsVsNode.jpg";
 const Blogs = () => {
   return (
     <div className="">
-      <div className="container  d-flex justify-content-center align-items-center">
-        <div className="vsImage bg-clr p-5 my-5 d-flex">
+      <div className="container">
+        <div>
+        <h3 className='text-center my-5 fw-bold blogHeader'>Blogs</h3>
+        <div className="vsImage bg-clr rounded p-5 my-5 d-flex">
           <img src={vsImg} alt="" />
           <div className="vImgContainer">
             <h3>1. Difference between javascript and node js ?</h3>
@@ -41,6 +43,121 @@ const Blogs = () => {
             </div>
             </div>
           </div>
+        </div>
+        </div>
+        <div>
+        <div className="vsImage bg-clr rounded p-5 my-5 d-flex">
+          <img src={vsImg} alt="" />
+          <div className="vImgContainer">
+            <h3>1. Difference between javascript and node js ?</h3>
+            <div className='d-flex blog-text'>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div>
+        <div className="vsImage bg-clr rounded p-5 my-5 d-flex">
+          <img src={vsImg} alt="" />
+          <div className="vImgContainer">
+            <h3>1. Difference between javascript and node js ?</h3>
+            <div className='d-flex blog-text'>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        <div>
+        <div className="vsImage bg-clr rounded p-5 my-5 d-flex">
+          <img src={vsImg} alt="" />
+          <div className="vImgContainer">
+            <h3>1. Difference between javascript and node js ?</h3>
+            <div className='d-flex blog-text'>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            <div>
+              <h5>JavaScript</h5>
+              <p>
+                Javascript is a programming language that is used for writing
+                scripts on the website. 2) Javascript can only be run in the
+                browsers. 3)It is basically used on the client-side.
+                4)Javascript is capable enough to add HTML and play with the
+                DOM. 5) Javascript can run in any browser engine as like JS core
+                in safari and Spidermonkey in Firefox. 6)Javascript is used in
+                frontend development. 7) Some of the javascript frameworks are
+                RamdaJS, TypedJS, etc. 8) It is the upgraded version of ECMA
+                script that uses Chrome’s V8 engine written in C++.
+              </p>
+            </div>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,68 @@
 import React from "react";
 import "./Footer.css";
+import { AiFillFacebook } from "react-icons/ai";
 
 const Footer = () => {
   const dynamicYear = new Date().getFullYear();
   return (
-    <div className="background-clr text-white">
-      <p className='container text-center p-3'>© all rights reserved copyright abidislamabir@gmail.com</p>
+    <div className="bg-dark text-white pt-5 pb-4">
+      <div className="container text-md-left">
+        <div className="row text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className=" text-uppercase mb-4 w-bold text-warning">
+              Company Name
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+              voluptatibus est ad placeat, fugiat, vero inventore magni
+              doloribus enim maxime reprehenderit asperiores sapiente voluptas
+              perferendis temporibus sint nihil atque. Minus.
+            </p>
+            <div className='svgIcon'>
+            <a href="##"> <AiFillFacebook/> </a>
+            <a href="##"> <AiFillFacebook/> </a>
+            <a href="##"> <AiFillFacebook/> </a>
+            <a href="##"> <AiFillFacebook/> </a>
+            </div>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 w-bold text-warning">
+              Products
+            </h5>
+            <div>
+               <p>Blogs</p>
+               <p>Pricing</p>
+               <p>Sales</p>
+               <p>Tickets</p>
+               <p>Certification</p>
+               <p>Customer Service</p>
+            </div>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 w-bold text-warning">
+              Useful Links
+            </h5>
+            <div>
+               <p>Blogs</p>
+               <p>Pricing</p>
+               <p>Sales</p>
+               <p>Tickets</p>
+               <p>Certification</p>
+               <p>Customer Service</p>
+            </div>
+          </div>
+          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 w-bold text-warning">
+              Address
+            </h5>
+            <p>127, Westwood Land</p>
+            <p>DA15 9PS, Sidecup</p>
+            <p>London, UK</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <p className='container text-center p-3'>© all rights reserved copyright abidislamabir@gmail.com</p> */}
     </div>
   );
 };
